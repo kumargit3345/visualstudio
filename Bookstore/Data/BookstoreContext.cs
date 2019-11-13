25ee036e2510ee91ff845c7ef126a57c69a5ec23
Nithin Gaddam <ngaddam@alluma.org>
2019-11-12 21:48:06 -0800

    All changes

---
+++ Bookstore/Data/BookstoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore.Models
+{
+    public class BookstoreContext : DbContext
+    {
+        public BookstoreContext (DbContextOptions<BookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bookstore.Models.Book> Book { get; set; }
+    }
+}
